8fee036a49da5609f5b5cda5cb01dc6c75696e41
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-07 22:32:50 +0700

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -24,5 +24,5 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 :--------------------------------------
-net stop KigyoRobot
-net start KigyoRobot
+net stop YuiiRobot
+net start YuiiRobot

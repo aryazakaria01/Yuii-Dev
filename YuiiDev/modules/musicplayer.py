054e7771c523372cee4f6516f350001a5b9f2433
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-08 13:53:59 +0700

    Create musicplayer.py

---
+++ YuiiDev/modules/musicplayer.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2021 aryazakaria01
+
+
+# This file is part of aryazakaria01 (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__mod_name__ = "Music Player"
+__help__ = """
+          <b>》** YuiiXMusic ** 《</b>
+            
+ OdaXMusic plays plays music in your group's voice chat
+ 
+ **Assistant Name** >> @SaitamaHelper
+╔═════════════════════════════════
+╠═1). Make bot admin
+╠═2). Start a voice chat
+╠═3). Try /play [song name] for the first time by an admin
+╠═4). If userbot joined enjoy music, If not add @OdaHelper 
+╠═════════════════════════════════
+╠═/play [title] - Play song using youtube music
+╠═/play | /ytplay [yt url] - Play the given yt url
+╠═/dplay [title] - Play song using deezer music
+╠═/splay [title] - Play song using svaan music
+╠═════════════════════════════════
+╠═/skip - Skips the current track
+╠═/pause - Pause track
+╠═/resume - Resumes the paused track
+╠═/end - Stops media playback
+╠═/current - Shows the current Playing track
+╠═/playlist - Shows playlist
+╠═/player - Open Settings menu of player
+╠═════════════════════════════════
+╠═/admincache - Updates admin info of your group
+╠═/userbotjoin: Invite @SaitamaHelper Userbot to your chat
+╚═════════════════════════════════
+
+NOTE : Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
+ PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME
+"""

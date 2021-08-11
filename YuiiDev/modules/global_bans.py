0c5854d8840a386b23aa554c952a510a852b8a80
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-08-11 22:54:13 +0700

    LMAO
    Signed-off-by: Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>

--- YuiiDev/modules/global_bans.py
+++ YuiiDev/modules/global_bans.py
@@ -552,11 +552,11 @@ Constantly help banning spammers off from your group automatically So, you wont 
 *Note:* Users can appeal spamwatch bans at @SpamwatchSupport
 """
 
-GBAN_HANDLER = CommandHandler("gban", gban)
-UNGBAN_HANDLER = CommandHandler("ungban", ungban)
-GBAN_LIST = CommandHandler("gbanlist", gbanlist)
-GBAN_STATUS = CommandHandler("antispam", gbanstat, filters=Filters.chat_type.groups)
-GBAN_ENFORCER = MessageHandler(Filters.all & Filters.chat_type.groups, enforce_gban)
+GBAN_HANDLER = CommandHandler("gban", gban, run_async=True)
+UNGBAN_HANDLER = CommandHandler("ungban", ungban, run_async=True)
+GBAN_LIST = CommandHandler("gbanlist", gbanlist, run_async=True)
+GBAN_STATUS = CommandHandler("antispam", gbanstat, filters=Filters.chat_type.groups, run_async=True)
+GBAN_ENFORCER = MessageHandler(Filters.all & Filters.chat_type.groups, enforce_gban, run_async=True)
 
 dispatcher.add_handler(GBAN_HANDLER)
 dispatcher.add_handler(UNGBAN_HANDLER)
